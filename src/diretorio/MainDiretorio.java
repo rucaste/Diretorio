bb907f17fed75bf1bff3a84f531907835df434b2
Rui Castelhano <rmmco11@iscte-iul.pt>
2018-12-17 12:19:23 +0000

    Final?

--- src/diretorio/MainDiretorio.java
+++ src/diretorio/MainDiretorio.java
@@ -10,7 +10,7 @@ public class MainDiretorio {
 			System.exit(1);
 		}
 		else if(args.length > 1){
-			System.out.println("Os argumentos a seguir ao primeiro foram ignorados");
+			System.out.println("Os argumentos a seguir ao primeiro foram ignorados ");
 		}
 		Diretorio diretorio = new Diretorio(Integer.parseInt(args[0]));
 		diretorio.startDiretorio();
